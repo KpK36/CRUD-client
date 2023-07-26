575118fb2e7aed579bbdda7a7dd318ad1b3c1aff
KpK36 <kayopatryck1@gmail.com>
2023-07-26 00:58:28 -0300

    Criação ClientController e retorno de todos os clientes paginados

---
+++ src/main/java/com/devsuperior/crudclient/controller/ClientController.java
@@ -0,0 +1,26 @@
+package com.devsuperior.crudclient.controller;
+
+import com.devsuperior.crudclient.dto.ClientDTO;
+import com.devsuperior.crudclient.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/clients")
+public class ClientController {
+
+    @Autowired
+    ClientService clientService;
+
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable) {
+        return ResponseEntity.ok(clientService.findAll(pageable));
+
+    }
+
+}
