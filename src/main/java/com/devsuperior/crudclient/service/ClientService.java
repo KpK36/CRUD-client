b05fadd0db2f9b4ab4a0ac0fefbc8a401b443fc9
KpK36 <kayopatryck1@gmail.com>
2023-07-26 20:07:22 -0300

    Change insert e tratamento para lista vazia de clientes

--- src/main/java/com/devsuperior/crudclient/service/ClientService.java
+++ src/main/java/com/devsuperior/crudclient/service/ClientService.java
@@ -3,13 +3,14 @@ package com.devsuperior.crudclient.service;
 import com.devsuperior.crudclient.dto.ClientDTO;
 import com.devsuperior.crudclient.entity.Client;
 import com.devsuperior.crudclient.repository.ClientRespository;
+import com.devsuperior.crudclient.service.exception.ClientsNotFoundException;
 import com.devsuperior.crudclient.service.exception.ResourceNotFoundException;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Optional;
 
@@ -20,7 +21,11 @@ public class ClientService {
     ClientRespository clientRespository;
     @Transactional
     public Page<ClientDTO> findAll(Pageable pageable){
-        return clientRespository.findAll(pageable).map(client -> new ClientDTO(client));
+
+        if (clientRespository.findAll().isEmpty())
+            throw new ClientsNotFoundException("Não foi cadastrado nenhum cliente");
+
+        return clientRespository.findAll(pageable).map(ClientDTO::new);
 
     }
 
@@ -29,6 +34,20 @@ public class ClientService {
         return new ClientDTO(clientRespository.findById(id).orElseThrow(
                 () -> new ResourceNotFoundException("Id não encontrado")
         ));
+
+    }
+
+    @Transactional
+    public ClientDTO insert(ClientDTO clientDTO) {
+
+        Client client = new Client();
+        client.setName(clientDTO.getName());
+        client.setCpf(clientDTO.getCpf());
+        client.setIncome(clientDTO.getIncome());
+        client.setBirthDate(clientDTO.getBirthDate());
+        client.setChildren(clientDTO.getChildren());
+
+        return new ClientDTO(clientRespository.save(client));
 
     }
 
