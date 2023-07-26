ef6df171f4b1e56ba79e07e9ea5e5fed974714e2
KpK36 <kayopatryck1@gmail.com>
2023-07-26 20:07:48 -0300

    Bean Validation e adição de setters

--- src/main/java/com/devsuperior/crudclient/dto/ClientDTO.java
+++ src/main/java/com/devsuperior/crudclient/dto/ClientDTO.java
@@ -1,15 +1,20 @@
 package com.devsuperior.crudclient.dto;
 
 import com.devsuperior.crudclient.entity.Client;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+
 
 import java.time.LocalDate;
 
 public class ClientDTO {
 
     private Long id;
+    @NotBlank(message = "Campo requerido")
     private String name;
     private String cpf;
     private Double income; // renda
+    @PastOrPresent(message = "Não pode ser data futura")
     private LocalDate birthDate;
     private Integer children;
 
@@ -20,6 +25,34 @@ public class ClientDTO {
         income = client.getIncome();
         birthDate = client.getBirthDate();
         children = client.getChildren();
+    }
+
+    public ClientDTO(){
+
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setIncome(Double income) {
+        this.income = income;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public void setChildren(Integer children) {
+        this.children = children;
     }
 
     public Long getId() {
