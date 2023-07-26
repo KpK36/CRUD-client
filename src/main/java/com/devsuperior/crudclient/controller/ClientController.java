d83babf234c851dac9e08cfe72e5d9a7be344a23
KpK36 <kayopatryck1@gmail.com>
2023-07-26 20:08:15 -0300

    Controller Inserção de novo cliente

--- src/main/java/com/devsuperior/crudclient/controller/ClientController.java
+++ src/main/java/com/devsuperior/crudclient/controller/ClientController.java
@@ -2,14 +2,16 @@ package com.devsuperior.crudclient.controller;
 
 import com.devsuperior.crudclient.dto.ClientDTO;
 import com.devsuperior.crudclient.service.ClientService;
+import jakarta.validation.Valid;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/clients")
@@ -28,6 +30,16 @@ public class ClientController {
     @GetMapping("/{id}")
     public ResponseEntity<ClientDTO> findById (@PathVariable Long id) {
         return ResponseEntity.ok(clientService.findById(id));
+
+    }
+
+    @PostMapping
+    public ResponseEntity<ClientDTO> insert (@Valid @RequestBody ClientDTO clientDTO){
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(clientDTO.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(clientService.insert(clientDTO));
 
     }
 
