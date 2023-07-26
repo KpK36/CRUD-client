6f6cb86cfd89af318efad461395a8b7649546027
KpK36 <kayopatryck1@gmail.com>
2023-07-26 02:10:09 -0300

    Change retorno do método FindById

--- src/main/java/com/devsuperior/crudclient/service/ClientService.java
+++ src/main/java/com/devsuperior/crudclient/service/ClientService.java
@@ -1,14 +1,17 @@
 package com.devsuperior.crudclient.service;
 
-import ch.qos.logback.core.net.server.Client;
 import com.devsuperior.crudclient.dto.ClientDTO;
+import com.devsuperior.crudclient.entity.Client;
 import com.devsuperior.crudclient.repository.ClientRespository;
+import com.devsuperior.crudclient.service.exception.ResourceNotFoundException;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ClientService {
@@ -18,6 +21,14 @@ public class ClientService {
     @Transactional
     public Page<ClientDTO> findAll(Pageable pageable){
         return clientRespository.findAll(pageable).map(client -> new ClientDTO(client));
+
+    }
+
+    @Transactional
+    public ClientDTO findById (Long id) {
+        return new ClientDTO(clientRespository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Id não encontrado")
+        ));
 
     }
 
